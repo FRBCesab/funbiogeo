f8e308a725f404e00d8fa0baa7a0d3d912d329a4
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-09-08 17:18:45 +0200

    Working get coverage draft tests

--- R/fbg_get_coverage.R
+++ R/fbg_get_coverage.R
@@ -1,5 +1,10 @@
 #' Compute trait coverage
-#'
+#' 
+#' Compute trait coverage for all sites, i.e., percent of total
+#' abundance/presence of species that have trait data compared to total species.
+#' This function assumes consider that all species provided in the trait dataset
+#' have all their traits specified.
+#' 
 #' @param site_sp 
 #' @param traits 
 #'
@@ -16,7 +21,23 @@ fbg_get_coverage = function(site_sp, traits) {
   check_traits(traits)
   
   # Get species in common between both matrices
-  species = list_common_species(site_sp, traits)
+  species = list_common_species(colnames(site_sp), rownames(traits))
   
-  return(site_sp)
+  # Subset data with common species
+  traits  = traits[species,]
+  
+  # Count all species per site
+  site_total_abundance = rowSums(site_sp, na.rm = TRUE)
+  
+  # Count species with traits per site
+  site_cover_abundance = rowSums(site_sp[, species], na.rm = TRUE)
+  
+  # Compute trait coverage
+  trait_coverage = site_cover_abundance/site_total_abundance
+  
+  # Transforming into tidy format
+  trait_coverage = stack(trait_coverage)
+  colnames(trait_coverage) = c("trait_coverage", "site")
+  
+  return(trait_coverage[, c(2, 1)])
 }
---
+++ tests/testthat/test-fbg_get_coverage.R
@@ -0,0 +1,14 @@
+test_that("fgb_get_coverage() works", {
+  # Wrong input
+  
+  # No species in common
+  
+  # Trait with NAs
+  
+  # Occurrence matrix
+  
+  # Abundance matrix
+  
+  # Coverage matrix
+  
+})
