c3bea6e63a9955db4b5c48903d8758d077dac023
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2021-09-13 18:13:10 +0200

    Working version and tests of CWM

--- R/fbg_cwm.R
+++ R/fbg_cwm.R
@@ -15,13 +15,27 @@ fbg_cwm = function(site_sp, traits) {
   check_traits(traits)
   
   # Get species in common between both matrices
-  species = species_in_common(site_sp, traits)
+  species = list_common_species(colnames(site_sp), rownames(traits))
   
   # Select quantitative traits for CWM
-  quanti_traits = traits
+  quanti_traits = apply(traits, 2, is.numeric)
+  traits = traits[, quanti_traits, drop = FALSE]
+  
+  if (identical(as.logical(traits), logical(0))) {
+    stop("CWM can only be computed on numeric traits", call. = FALSE)
+  }
+  
+  # Total sites abundances
+  total_abund = rowSums(site_sp[, species, drop = FALSE])
   
   # Compute CWM
-  cwm = site_sp[, species] %*% quanti_traits[species,]
+  cwm = (site_sp[, species, drop = FALSE]/total_abund) %*%
+    traits[species,, drop = FALSE]
   
   # Reformat CWM in good way (tidy format)
+  data.frame(
+    site  = rownames(cwm),
+    trait = colnames(cwm),
+    cwm   = as.numeric(t(cwm))
+  )
 }
--- tests/testthat/test-fbg_cwm.R
+++ tests/testthat/test-fbg_cwm.R
@@ -1,3 +1,41 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("fbg_cwm() works", {
+  # Wrong input
+  expect_error(
+    fbg_cwm(data.frame("a"), data.frame("a")),
+    "Provided site-species object is not a numeric matrix or a data.frame",
+    fixed = TRUE
+  )
+  
+  # No species in common
+  expect_error(
+    fbg_cwm(
+      site_sp = matrix(1, dimnames = list("s1", "a")),
+      traits  = matrix(1, dimnames = list("b", "t1"))
+    ),
+    "No species found in common between inputs",
+    fixed = TRUE
+  )
+  
+  # No numeric traits
+  expect_error(
+    fbg_cwm(
+      site_sp = matrix(1, dimnames = list("s1", "a")),
+      traits  = matrix("a", dimnames = list("a", "t1"))
+    ),
+    "CWM can only be computed on numeric traits",
+    fixed = TRUE
+  )
+  
+  # Valid input
+  expect_silent(
+    test_cwm <- fbg_cwm(
+      site_sp = matrix(1, dimnames = list("s1", "a")),
+      traits  = matrix(1, dimnames = list("a", "t1"))
+    )
+  )
+  
+  expect_s3_class(test_cwm, "data.frame")
+  expect_named(test_cwm, c("site", "trait", "cwm"))
+  expect_equal(dim(test_cwm), c(1, 3))
+  expect_equal(test_cwm$cwm, 1)
 })
