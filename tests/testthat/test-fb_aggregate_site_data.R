f327ac684d13fe5443e772fd5dfdf8650dc1daeb
Nicolas Casajus <nicolas.casajus@gmail.com>
2021-11-17 09:04:31 +0100

    Adapt tests (with new objects names)

--- tests/testthat/test-fb_aggregate_site_data.R
+++ tests/testthat/test-fb_aggregate_site_data.R
@@ -1,13 +1,13 @@
 # Initial data -----------------------------------------------------------------
-data("site_locs")
-data("species_occs")
+data("site_locations")
+data("site_species")
 
 # Convert site locations in to an 'sf' object
-site_locs <- site_locs[!duplicated(site_locs),]
-site_locs[["site"]] <- rownames(site_locs)
-rownames(site_locs) <- NULL
-site_locs <- sf::st_as_sf(
-  site_locs, coords = 1:2, crs = 4326
+site_locations <- site_locations[!duplicated(site_locations),]
+site_locations[["site"]] <- rownames(site_locations)
+rownames(site_locations) <- NULL
+site_locations <- sf::st_as_sf(
+  site_locations, coords = 1:2, crs = 4326
 )
 
 
@@ -28,13 +28,13 @@ test_that("fb_aggregate_site_data() errors with missing input", {
   )
   
   expect_error(
-    fb_aggregate_site_data(site_locs),
+    fb_aggregate_site_data(site_locations),
     "Argument 'site_data' is required",
     fixed = TRUE
   )
   
   expect_error(
-    fb_aggregate_site_data(site_locs, species_occs),
+    fb_aggregate_site_data(site_locations, site_species),
     "Argument 'agg_grid' is required",
     fixed = TRUE
   )
@@ -46,23 +46,23 @@ test_that("fb_aggregate_site_data() errors with wrong input", {
   
   # Wrong 'site_data' argument
   expect_error(
-    fb_aggregate_site_data(site_locs, as.list(species_occs)),
+    fb_aggregate_site_data(site_locations, as.list(site_species)),
     "Argument 'site_data' must be a matrix or a data.frame",
     fixed = TRUE
   )
   
   expect_error(
-    fb_aggregate_site_data(site_locs, 
-                     species_occs[-c(seq_len(nrow(species_occs))), ]),
+    fb_aggregate_site_data(site_locations, 
+                           site_species[-c(seq_len(nrow(site_species))), ]),
     "Argument 'site_data' should have at least one row and one column",
     fixed = TRUE
   )
   
-  data_test <- as.data.frame(species_occs)
+  data_test <- as.data.frame(site_species)
   rownames(data_test) <- NULL
   
   expect_error(
-    fb_aggregate_site_data(site_locs, data_test),
+    fb_aggregate_site_data(site_locations, data_test),
     "Argument 'site_data' must have row names (sites names)",
     fixed = TRUE
   )
@@ -70,16 +70,16 @@ test_that("fb_aggregate_site_data() errors with wrong input", {
   data_test <- data.matrix(data_test)
   
   expect_error(
-    fb_aggregate_site_data(site_locs, data_test),
+    fb_aggregate_site_data(site_locations, data_test),
     "Argument 'site_data' must have row names (sites names)",
     fixed = TRUE
   )
   
-  data_test <- species_occs
+  data_test <- site_species
   data_test <- data.frame(data_test, test = rep("A", nrow(data_test)))
   
   expect_error(
-    fb_aggregate_site_data(site_locs, data_test),
+    fb_aggregate_site_data(site_locations, data_test),
     paste0("Argument 'site_data' must contain only numeric values. ",
            "Sites names must be provided as row names"),
     fixed = TRUE
@@ -89,7 +89,7 @@ test_that("fb_aggregate_site_data() errors with wrong input", {
   grid_test <- raster::raster(tavg_file)
   
   expect_error(
-    fb_aggregate_site_data(site_locs, species_occs, grid_test),
+    fb_aggregate_site_data(site_locations, site_species, grid_test),
     "The 'agg_grid' raster must be a 'SpatRaster' object (package `terra`)",
     fixed = TRUE
   )
@@ -98,7 +98,7 @@ test_that("fb_aggregate_site_data() errors with wrong input", {
   terra::crs(grid_test) <- NA
   
   expect_error(
-    fb_aggregate_site_data(site_locs, species_occs, grid_test),
+    fb_aggregate_site_data(site_locations, site_species, grid_test),
     "The 'agg_grid' raster must have a CRS (coordinate system)",
     fixed = TRUE
   )
@@ -110,12 +110,12 @@ test_that("fb_aggregate_site_data() errors with wrong input", {
 test_that("fb_aggregate_site_data() works", {
   
   # No reprojection
-  expect_silent(ras <- fb_aggregate_site_data(site_locs, species_occs[ , 1:2], tavg))
+  expect_silent(ras <- fb_aggregate_site_data(site_locations, site_species[ , 1:2], tavg))
   
   expect_s4_class(ras, "SpatRaster")
-  expect_named(ras, c("acer_negundo", "acer_nigrum"))
-  expect_equal(dim(ras), c(61, 139, 2))
-  expect_equal(ras[][1401], 0, tolerance = 0.000001)
+  expect_named(ras, c("sp_001", "sp_002"))
+  expect_equal(dim(ras), c(290, 405, 2))
+  expect_equal(ras[][1816], 1, tolerance = 0.000001)
   
   
   # Change projection of rasters
@@ -123,9 +123,9 @@ test_that("fb_aggregate_site_data() works", {
   
   tavg_prj <- terra::project(tavg, rob)
   
-  expect_silent(ras <- fb_aggregate_site_data(site_locs, species_occs[ , 1:2], 
+  expect_silent(ras <- fb_aggregate_site_data(site_locations, site_species[ , 1:2], 
                                         tavg_prj))
   
   expect_s4_class(ras, "SpatRaster")
-  expect_named(ras, c("acer_negundo", "acer_nigrum"))
+  expect_named(ras, c("sp_001", "sp_002"))
 })
--- tests/testthat/test-fb_get_environment.R
+++ tests/testthat/test-fb_get_environment.R
@@ -1,13 +1,13 @@
 # Initial data -----------------------------------------------------------------
 
 # Site x locations object
-data("site_locs")
+data("site_locations")
 
-site_locs <- site_locs[!duplicated(site_locs),]
-site_locs[["site"]] <- rownames(site_locs)
-rownames(site_locs) <- NULL
-site_locs <- sf::st_as_sf(
-  site_locs, coords = 1:2, crs = 4326
+site_locations <- site_locations[!duplicated(site_locations),]
+site_locations[["site"]] <- rownames(site_locations)
+rownames(site_locations) <- NULL
+site_locations <- sf::st_as_sf(
+  site_locations, coords = 1:2, crs = 4326
 )
 
 # Environmental rasters
@@ -34,7 +34,7 @@ test_that("fb_get_environment() errors with missing input", {
   )
   
   expect_error(
-    fb_get_environment(site_locations = site_locs),
+    fb_get_environment(site_locations = site_locations),
     "Argument 'environment_raster' (environmental raster) is required",
     fixed = TRUE
   )
@@ -47,26 +47,26 @@ test_that("fb_get_environment() errors with wrong input type", {
   
   # Wrong site x locations object
   expect_error(
-    fb_get_environment(site_locs[[1]], layers),
+    fb_get_environment(site_locations[[1]], layers),
     "The site x locations object must be an 'sf' object",
     fixed = TRUE
   )
   
   expect_error(
-    fb_get_environment(as.list(site_locs), layers),
+    fb_get_environment(as.list(site_locations), layers),
     "The site x locations object must be an 'sf' object",
     fixed = TRUE
   )
   
   expect_error(
-    fb_get_environment(site_locs[-c(seq_len(nrow(site_locs))), ], layers),
+    fb_get_environment(site_locations[-c(seq_len(nrow(site_locations))), ], layers),
     "The site x locations object should have at least one row",
     fixed = TRUE
   )
   
   # Wrong environmental raster type
   expect_error(
-    fb_get_environment(site_locs, raster::stack(c(tavg, prec))),
+    fb_get_environment(site_locations, raster::stack(c(tavg, prec))),
     "The raster layer must be a 'SpatRaster' object (package `terra`)",
     fixed = TRUE
   )
@@ -78,22 +78,22 @@ test_that("fb_get_environment() errors with wrong input type", {
 test_that("fb_get_environment() works", {
   
   # Regular input
-  expect_silent(env_value <- fb_get_environment(site_locs, layers))
+  expect_silent(env_value <- fb_get_environment(site_locations, layers))
   
   expect_s3_class(env_value, "data.frame")
   expect_named(env_value, c("site", "annual_mean_temp", "annual_tot_prec"))
-  expect_equal(dim(env_value), c(3265, 3))
-  expect_equal(env_value[["annual_tot_prec"]][[1]], 1121)
+  expect_equal(dim(env_value), c(1505, 3))
+  expect_equal(env_value[["annual_tot_prec"]][[1]], 2620)
   
   # Different CRS
   rob <- "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
   
   layers_prj <- terra::project(layers, rob)
   
-  expect_silent(env_value <- fb_get_environment(site_locs, layers_prj))
+  expect_silent(env_value <- fb_get_environment(site_locations, layers_prj))
   
   expect_s3_class(env_value, "data.frame")
   expect_named(env_value, c("site", "annual_mean_temp", "annual_tot_prec"))
-  expect_equal(dim(env_value), c(3265, 3))
-  expect_equal(env_value[["annual_mean_temp"]][[1]], 11.0539, tolerance = 0.01)
+  expect_equal(dim(env_value), c(1505, 3))
+  expect_equal(env_value[["annual_mean_temp"]][[1]], 6.3, tolerance = 0.01)
 })
