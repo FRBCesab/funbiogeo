94253d161990e950b0a07480ffa223b3444cd63f
Rekyt <matthias.grenie@ens-lyon.fr>
2021-12-23 11:42:14 +0100

    Reorganized tests

--- tests/testthat/test-fb_cwm.R
+++ tests/testthat/test-fb_cwm.R
@@ -1,20 +1,31 @@
-test_that("fb_cwm() works", {
-  
-  site_species  <- data.frame(
-    site = letters[1:5],
-    sp1  = c(0, 0, 10, 0, 0),
-    sp2  = c(1, 10, 0, 25, 40),
-    sp3  = c(22, 8, 3, 0, 12),
-    sp4  = c(3, 0, 2, 12, 0)
-  )
-  species_traits <- data.frame(
-    species = paste0("sp", 1:4),
-    t1      = c(1.1, 2.5, 100, 400)
-  )
-  
-  
-  # Wrong inputs ----
+# Preliminary data -------------------------------------------------------------
 
+site_species  <- data.frame(
+  site = letters[1:5],
+  sp1  = c(0, 0, 10, 0, 0),
+  sp2  = c(1, 10, 0, 25, 40),
+  sp3  = c(22, 8, 3, 0, 12),
+  sp4  = c(3, 0, 2, 12, 0)
+)
+species_traits <- data.frame(
+  species = paste0("sp", 1:4),
+  t1      = c(1.1, 2.5, 100, 400)
+)
+
+# Data with non numeric traits
+species_traits_2 <- data.frame(
+  species = paste0("sp", 1:4),
+  t1      = c(1.1, 2.5, 100, 400),
+  t2      = c("a", "a", "b", "b"),
+  t3      = factor(c("a", "a", "b", "b")),
+  t4      = ordered(c("a", "a", "b", "b"))
+)
+
+
+# Wrong Inputs -----------------------------------------------------------------
+
+test_that("fb_cwm() fails with wrong inputs", {
+  
   expect_error(
     fb_cwm(species_traits = species_traits),
     "Argument 'site_species' (site x species matrix) is required",
@@ -72,7 +83,7 @@ test_that("fb_cwm() works", {
   )
   
   
-  # No species in common ----
+  # No species in common
   
   expect_error(
     fb_cwm(site_species[,1:3], species_traits[3:4,]),
@@ -81,16 +92,20 @@ test_that("fb_cwm() works", {
   )
   
   
-  # No numeric traits ----
+  # No numeric traits
   
   expect_error(
     fb_cwm(site_species, species_traits[, 1, drop = FALSE]),
     "CWM can only be computed on numeric traits",
     fixed = TRUE
   )
-  
-  
-  # Valid input ----
+
+})
+
+
+# Valid Inputs -----------------------------------------------------------------
+
+test_that("fb_cwm() works with valid inputs", {
   
   expect_silent(test_cwm <- fb_cwm(site_species, species_traits))
   
