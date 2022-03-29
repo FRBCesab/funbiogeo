39a823afdfc5cf923f558eca7e512a2d5fcc4d51
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-03-29 16:52:03 +0200

    Sticker - Attempt 2

--- inst/sticker/funbiogeo-sticker.R
+++ inst/sticker/funbiogeo-sticker.R
@@ -47,25 +47,14 @@ p <- ggplot2::ggplot() +
   ggplot2::coord_sf(ylim = c(-6145789, 8611877), expand = FALSE, crs  = prj,
                     clip = "off") +
   
-  rphylopic::add_phylopic(sil, 1,        0, -6000000, ysize =  750000, color = "#46125C") + 
-  rphylopic::add_phylopic(sil, 1,  4000000, -6000000, ysize = 1500000, color = "#1C9587") + 
-  rphylopic::add_phylopic(sil, 1,  9000000, -6000000, ysize = 3000000, color = "#DCE24D") + 
-  
-  # ggplot2::geom_segment(ggplot2::aes(x    = -15000000, y    = -1250000, 
-  #                                    xend = -15000000, yend = -4500000),
-  #                       
-  #                       arrow = ggplot2::arrow(length = ggplot2::unit(0.1, "cm"), 
-  #                                              ends = "last", type = "closed"), 
-  #                       lwd = 0.3) +
-  # ggplot2::geom_segment(ggplot2::aes(x    = -15000000, y    = 3250000,
-  #                                    xend = -15000000, yend = 6500000),
-  #                       arrow = ggplot2::arrow(length = ggplot2::unit(0.1, "cm"),
-  #                                              ends = "last", type = "closed"),
-  #                       lwd = 0.3) +
+  # rphylopic::add_phylopic(sil, 1,        0, -6000000, ysize =  780000, color = "white") + 
+  rphylopic::add_phylopic(sil, 1,        0, -6000000, ysize =  750000, color = "#266E8C") + 
+  rphylopic::add_phylopic(sil, 1,  4000000, -6000000, ysize = 1500000, color = "#55C968") + 
+  rphylopic::add_phylopic(sil, 1,  9000000, -6000000, ysize = 3000000, color = "#FFE740") + 
 
   ggplot2::geom_segment(ggplot2::aes(x    =   -500000, y    = -7900000,
                                      xend =  10550000, yend = -7900000),
-                        arrow = ggplot2::arrow(length = ggplot2::unit(0.00, "cm"),
+                        arrow = ggplot2::arrow(length = ggplot2::unit(0.05, "cm"),
                                                ends = "last", type = "closed"),
                         lwd = 0.1, color = "white") +
   ggplot2::scale_fill_viridis_c() +
@@ -84,13 +73,13 @@ hexSticker::sticker(
   filename  = here::here("man", "figures", "funbiogeo-sticker.png"),
   dpi       = 2400,
 
-  p_size    = 135.0,         # Title
-  u_size    =  25.0,         # URL
+  p_size    = 140.0,         # Title
+  u_size    =  32.0,         # URL
   p_family  = "Aller_Rg",
 
   p_color   = "#ffffff",   # Title
   h_fill    = "#000000",   # Background
-  h_color   = "#1E7287",   # Border
+  h_color   = "#226D88",   # Border
   u_color   = "#ffffff",   # URL
 
   p_x       = 1.00,        # Title
@@ -101,9 +90,9 @@ hexSticker::sticker(
   s_width   = 1.8,         # Subplot 
   s_height  = 1.8,         # Subplot
 
-  url       = "https://frbcesab.github.io/funbiogeo/",
+  url       = "https://github.com/frbcesab/",
 
-  spotlight = TRUE,
+  spotlight = FALSE,
   l_alpha   = 0.10,
   l_width   = 4,
   l_height  = 4
