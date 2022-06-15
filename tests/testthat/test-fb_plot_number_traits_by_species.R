25fabf40b907c39b5882d29489ded08a47dbb6a4
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-06-15 13:59:50 +0200

    Testing for user-specified thresholds

--- tests/testthat/test-fb_plot_number_traits_by_species.R
+++ tests/testthat/test-fb_plot_number_traits_by_species.R
@@ -2,4 +2,11 @@ test_that("fb_plot_number_traits_by_species works", {
   expect_silent(given_plot <- fb_plot_number_traits_by_species(species_traits))
   
   expect_s3_class(given_plot, "ggplot")
+  
+  
+  expect_silent(
+    given_plot <- fb_plot_number_traits_by_species(species_traits, 3)
+  )
+  
+  expect_s3_class(given_plot, "ggplot")
 })
