5aa6093d6b1ca7b259c30f2d5345db40ab69880f
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-06-15 11:18:55 +0200

    Split functions in different files

---
+++ R/fb_count_traits_by_species.R
@@ -0,0 +1,51 @@
+#' Compute traits coverage for each species
+#' 
+#' @description
+#' For each species computes the percentage of traits without `NA` (missing 
+#' trait values).
+#' 
+#' @inheritParams fb_get_coverage
+#'
+#' @return A three-column `data.frame` with:
+#' - `species`: the name of the species;
+#' - `n_traits`: the number of traits with non-missing value for the species;
+#' - `coverage`: the percentage of traits with non-missing value for the 
+#' species.
+#' 
+#' @export
+#'
+#' @examples
+#' library("funbiogeo")
+#' 
+#' data("species_traits")
+#' 
+#' trait_coverage_by_species <- fb_count_traits_by_species(species_traits)
+
+fb_count_traits_by_species <- function(species_traits) { 
+  
+  ## Check inputs ----
+  
+  if (missing(species_traits)) {
+    stop("Argument 'species_traits' (species x traits data frame) is required")
+  }
+  
+  check_species_traits(species_traits)
+  
+  
+  ## Compute traits coverage by species ----
+  
+  traits_coverage <- apply(species_traits[ , -1], 1, function(x)
+    sum(!is.na(x)))
+  
+  traits_coverage <- data.frame(
+    "species"    = species_traits[ , 1],
+    "n_traits"   = traits_coverage,
+    "coverage"   = traits_coverage / (ncol(species_traits) - 1))
+  
+  traits_coverage <- traits_coverage[order(traits_coverage$"coverage", 
+                                           decreasing = TRUE), ]
+  
+  rownames(traits_coverage) <- NULL
+  
+  traits_coverage
+}
---
+++ R/fb_filter_traits_by_species_coverage.R
@@ -0,0 +1,82 @@
+#' Filter traits with a given species coverage threshold
+#' 
+#' @description
+#' Selects traits (columns) for which the percentage of species without 
+#' `NA` (missing trait values) is higher than a threshold.
+#' 
+#' @param threshold_species_proportion a numeric of length 1 between 0 and 1.
+#' The percentage of species coverage threshold.
+#' 
+#' @inheritParams fb_get_coverage
+#'
+#' @return A subset of `species_traits` with traits covered by X% of species.
+#' 
+#' @export
+#'
+#' @examples
+#' library("funbiogeo")
+#' 
+#' data("species_traits")
+#' 
+#' species_traits <- fb_filter_traits_by_species_coverage(
+#'   species_traits,
+#'   threshold_species_proportion = 0.6)
+
+fb_filter_traits_by_species_coverage <- function(
+    species_traits, 
+    threshold_species_proportion = 0) {
+  
+  ## Check inputs ----
+  
+  if (missing(species_traits)) {
+    stop("Argument 'species_traits' (species x traits data frame) is required")
+  }
+  
+  check_species_traits(species_traits)
+  
+  if (!is.numeric(threshold_species_proportion) | 
+      threshold_species_proportion > 1 | threshold_species_proportion < 0) {
+    stop("Coverage threshold should be a numeric value >= 0 and <= 1",
+         call. = FALSE)
+  }
+  
+  
+  # Check for absence of variability in traits ----
+  
+  n_modalities  <- unlist(lapply(colnames(species_traits)[-1], function(x) 
+    length(unique(species_traits[[x]][!is.na(species_traits[[x]])]))))
+  
+  only_na_traits <- colnames(species_traits)[-1][which(n_modalities == 0)]
+  
+  if (length(only_na_traits)) {
+    message("Some traits have only NA values. ", 
+            "Maybe you would like to remove them.")
+  }
+  
+  unique_traits <- colnames(species_traits)[-1][which(n_modalities == 1)]
+  
+  if (length(unique_traits)) {
+    message("Some traits have no variability (one single value). ", 
+            "Maybe you would like to remove them.")
+  }
+  
+  
+  # Get species coverage for each trait ----
+  
+  species_coverage <- fb_count_species_by_traits(species_traits)
+  
+  
+  # Filter traits by species coverage ----
+  
+  selected_traits <- species_coverage[
+    which(species_coverage[["coverage"]] >= threshold_species_proportion), 
+    "trait"]
+  
+  
+  if (identical(selected_traits, character(0))) {
+    message("No trait has the specified species coverage threshold")
+  }
+  
+  species_traits[ , c(colnames(species_traits)[1], selected_traits), 
+                  drop = FALSE]
+}
