a556c7632770b4c0e88f8a8f212087c14b5f3ffb
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-06-15 11:18:55 +0200

    Split functions in different files

---
+++ R/fb_filter_species_by_traits_coverage.R
@@ -0,0 +1,70 @@
+#' Filter species with a given traits coverage threshold
+#' 
+#' @description
+#' Selects species (rows) for which the percentage of traits without 
+#' `NA` (missing trait values) is higher than a threshold.
+#' 
+#' @param threshold a numeric of length 1 between 0 and 1. The percentage of 
+#' traits coverage threshold.
+#' 
+#' @inheritParams fb_get_coverage
+#'
+#' @return A subset of `species_traits` with species covered by X% of traits.
+#' 
+#' @export
+#'
+#' @examples
+#' library("funbiogeo")
+#' 
+#' data("species_traits")
+#' 
+#' species_traits <- fb_filter_species_by_traits_coverage(species_traits,
+#'                                                        threshold = 0.6)
+
+fb_filter_species_by_traits_coverage <- function(species_traits, 
+                                                 threshold = 0) { 
+
+  ## Check inputs ----
+  
+  if (missing(species_traits)) {
+    stop("Argument 'species_traits' (species x traits data frame) is required")
+  }
+  
+  check_species_traits(species_traits)
+  
+  if (!is.numeric(threshold) | threshold > 1 | threshold < 0) {
+    stop("Coverage threshold should be a numeric value >= 0 and <= 1",
+         call. = FALSE)
+  }
+  
+  
+  # Check for absence of variability in traits ----
+  
+  n_modalities  <- apply(species_traits[ , -1], 1, function(x)
+    length(unique(x[!is.na(x)])))
+  
+  only_na_traits <- species_traits[which(n_modalities == 0), 1]
+  
+  if (length(only_na_traits)) {
+    message("Some species have only NA values for all traits. ", 
+            "Maybe you would like to remove them.")
+  }
+  
+  
+  # Get species coverage for each trait ----
+  
+  traits_coverage <- fb_count_traits_by_species(species_traits)
+  
+  
+  # Filter traits by species coverage ----
+  
+  selected_species <- traits_coverage[
+    which(traits_coverage[["coverage"]] >= threshold), "species"]
+  
+  
+  if (identical(selected_species, character(0))) {
+    message("No species has the specified traits coverage threshold")
+  }
+  
+  species_traits[species_traits[ , 1] %in% selected_species, , drop = FALSE]
+}
