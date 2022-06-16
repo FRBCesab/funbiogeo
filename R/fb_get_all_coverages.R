f9b8ef3eaa0b5330b004e8cd0b72db3960ca9745
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-06-16 21:42:30 +0200

    Add fb_get_all_coverages()

---
+++ R/fb_get_all_coverages.R
@@ -0,0 +1,41 @@
+#' Compute Trait Coverage per Site for All Traits
+#'
+#' @inheritParams fb_get_coverage
+#'
+#' @return a data.frame with a column with sites and one column per provided
+#'   trait giving its coverage (percent species per site, weighted by abundance
+#'   that have trait data)
+#' @export
+#'
+#' @examples
+#' fb_get_all_coverages(site_species, species_traits)
+fb_get_all_coverages = function(site_species, species_traits) {
+  
+  # Checks
+  check_site_species(site_species)
+  check_species_traits(species_traits)
+  
+  # Computing Trait Coverage per Site for all Traits
+  full_coverage = fb_get_coverage(site_species, species_traits)
+  colnames(full_coverage)[2] = "all_traits"
+  
+  # Trait by Trait
+  trait_coverage = lapply(colnames(species_traits)[-1], function(x) {
+    
+    single_trait_coverage = fb_get_coverage(
+      site_species, species_traits[, c("species", x)]
+    )
+    
+    colnames(single_trait_coverage)[2] = x
+    
+    return(single_trait_coverage)
+  })
+  
+  # Combine Trait by Trait Coverages
+  trait_coverage = Reduce(
+    function(...) merge(..., by = "site", all.x = TRUE), trait_coverage
+  )
+  
+  # Merge All Coverages (all traits and trait by trait)
+  merge(full_coverage, trait_coverage, by = "site")
+}
---
+++ tests/testthat/test-fb_get_all_coverages.R
@@ -0,0 +1,57 @@
+test_that("fb_get_all_coverages() errors with wrong arguments", {
+  
+  # Missing Arguments
+  
+  expect_error(
+    fb_get_all_coverages(),
+    "Argument 'site_species' (site x species data frame) is required",
+    fixed = TRUE
+  )
+  
+  expect_error(
+    fb_get_all_coverages(site_species),
+    "Argument 'species_traits' (species x traits data frame) is required",
+    fixed = TRUE
+  )
+  
+  
+  # Wrong arguments
+  
+  expect_error(
+    fb_get_all_coverages(data.frame()),
+    "The site x species object should have at least one row and one column",
+    fixed = TRUE
+  )
+  
+  expect_error(
+    fb_get_all_coverages(site_species, data.frame()),
+    "The species x traits object should have at least one row and one column",
+    fixed = TRUE
+  )
+  
+  expect_error(
+    fb_get_all_coverages(list()),
+    "The site x species object must be a data.frame",
+    fixed = TRUE
+  )
+  
+  expect_error(
+    fb_get_all_coverages(site_species, list()),
+    "The species x traits object must be a data.frame",
+    fixed = TRUE
+  )
+})
+
+
+test_that("fb_get_all_coverages() works well with good arguments", {
+  
+  expect_silent(res <- fb_get_all_coverages(site_species, species_traits))
+  
+  expect_s3_class(res, "data.frame")
+  expect_equal(dim(res), c(1505, 8))
+  expect_equal(
+    colnames(res), c("site", "all_traits", colnames(species_traits)[-1])
+  )
+  expect_equal(res[1, 2], 1)
+  expect_equal(res[2, 5], 9/10)
+})
