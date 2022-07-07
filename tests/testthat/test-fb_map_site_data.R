743255d61771496a775f660d08869ba236a07776
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-07-07 18:03:27 +0200

    Increase coverage on fb_map_site_data()

--- tests/testthat/test-fb_map_site_data.R
+++ tests/testthat/test-fb_map_site_data.R
@@ -10,6 +10,45 @@ site_lines <- sf::st_cast(site_locations, "MULTILINESTRING")
 
 
 # Tests ------------------------------------------------------------------------
+test_that("fb_map_site_data() errors with wrong input", {
+  
+  # Missing Arguments
+  expect_error(
+    res <- fb_map_site_data(site_locations),
+    "Argument 'site_data' (site info data.frame) is required",
+    fixed = TRUE
+  )
+  
+  # Wrong type
+  expect_error(
+    res <- fb_map_site_data(site_locations, matrix()),
+    "Argument 'site_data' must be a data.frame",
+    fixed = TRUE
+  )
+  
+  # Column 'site' not in data.frame
+  expect_error(
+    res <- fb_map_site_data(site_locations, data.frame()),
+    "\"site\" column should be in provided 'site_data'",
+    fixed = TRUE
+  )
+  
+  # Missing 'selected_col' argument
+  expect_error(
+    res <- fb_map_site_data(site_locations, data.frame(site = "a")),
+    "Argument 'selected_col' (name of selected column) is required",
+    fixed = TRUE
+  )
+  
+  # Specified 'selected_col' not in provided 'site_data'
+  expect_error(
+    res <- fb_map_site_data(site_locations, data.frame(site = "a", b = 1), "a"),
+    "Provided 'selected_col' should be in 'site_data'",
+    fixed = TRUE
+  )
+  
+})
+
 test_that("fb_map_site_data() works", {
   
   # Polygons
