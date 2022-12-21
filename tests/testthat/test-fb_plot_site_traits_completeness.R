c5ddc8c58eeaae1d2b874904cb7fb39e01012985
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-12-21 16:43:35 +0100

    Finish implementing argument

--- tests/testthat/test-fb_plot_site_traits_completeness.R
+++ tests/testthat/test-fb_plot_site_traits_completeness.R
@@ -4,4 +4,12 @@ test_that("fb_plot_site_traits_completeness works", {
   )
   
   expect_s3_class(given_plot, "ggplot")
+  
+  expect_silent(
+    given_plot <- fb_plot_site_traits_completeness(
+      site_species, species_traits, FALSE
+    )
+  )
+  
+  expect_s3_class(given_plot, "ggplot")
 })
--- tests/testthat/test-fb_plot_species_traits_completeness.R
+++ tests/testthat/test-fb_plot_species_traits_completeness.R
@@ -4,4 +4,11 @@ test_that("fb_plot_species_traits_completeness works", {
   )
   
   expect_s3_class(given_plot, "ggplot")
+  
+  # Without 'all_traits' added
+  expect_silent(
+    given_plot <- fb_plot_species_traits_completeness(species_traits, FALSE)
+  )
+  
+  expect_s3_class(given_plot, "ggplot")
 })
