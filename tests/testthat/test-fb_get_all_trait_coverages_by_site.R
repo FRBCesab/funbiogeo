410bdce9ad93cab625c0a9918722e53d8e7f33b7
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-12-21 16:17:08 +0100

    Implement 'all_traits' argument to get all trait coverages

--- tests/testthat/test-fb_get_all_trait_coverages_by_site.R
+++ tests/testthat/test-fb_get_all_trait_coverages_by_site.R
@@ -40,12 +40,22 @@ test_that("fb_get_all_trait_coverages_by_site() errors with wrong arguments", {
     "The species x traits object must be a data.frame",
     fixed = TRUE
   )
+  
+  expect_error(
+    fb_get_all_trait_coverages_by_site(site_species, species_traits, 1),
+    "Argument 'all_traits' should be TRUE or FALSE",
+    fixed = TRUE
+  )
+  
 })
 
 
-test_that("fb_get_all_trait_coverages_by_site() works well with good arguments", {
+test_that("fb_get_all_trait_coverages_by_site() works well with good arguments",{
   
-  expect_silent(res <- fb_get_all_trait_coverages_by_site(site_species, species_traits))
+  # With all traits
+  expect_silent(
+    res <- fb_get_all_trait_coverages_by_site(site_species, species_traits)
+  )
   
   expect_s3_class(res, "data.frame")
   expect_equal(dim(res), c(1505, 8))
@@ -54,4 +64,18 @@ test_that("fb_get_all_trait_coverages_by_site() works well with good arguments",
   )
   expect_equal(round(res[1, 2], 2), 0.73)
   expect_equal(round(res[2, 5], 2), 0.97)
+  
+  # Without all traits
+  expect_silent(
+    res <- fb_get_all_trait_coverages_by_site(
+      site_species, species_traits, FALSE
+    )
+  )
+  
+  expect_s3_class(res, "data.frame")
+  expect_equal(dim(res), c(1505, 7))
+  expect_equal(
+    colnames(res), c("site", colnames(species_traits)[-1])
+  )
+  expect_equal(round(res[2, 4], 2), 0.91)
 })
