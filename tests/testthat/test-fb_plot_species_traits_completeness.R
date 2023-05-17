8722acb6fc5cbdc06f336009cd0607421664969d
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2023-05-17 11:57:58 +0200

    Implement category facetting for species-traits completeness

--- tests/testthat/test-fb_plot_species_traits_completeness.R
+++ tests/testthat/test-fb_plot_species_traits_completeness.R
@@ -7,7 +7,9 @@ test_that("fb_plot_species_traits_completeness works", {
   
   # Without 'all_traits' added
   expect_silent(
-    given_plot <- fb_plot_species_traits_completeness(species_traits, FALSE)
+    given_plot <- fb_plot_species_traits_completeness(
+      species_traits, all_traits =  FALSE
+    )
   )
   
   expect_s3_class(given_plot, "ggplot")
@@ -20,15 +22,58 @@ test_that("fb_plot_species_traits_completeness works", {
   )
   
   expect_silent(
-    given_plot <- fb_plot_species_traits_completeness(example_traits, FALSE)
+    given_plot <- fb_plot_species_traits_completeness(
+      example_traits, all_traits = FALSE
+    )
   )
   
-  # Test that function works with single trait
+  # Test that function works with a single trait
   
   expect_silent(
     given_plot <- fb_plot_species_traits_completeness(
-      example_traits[, 1:2], FALSE
+      example_traits[, 1:2], all_traits = FALSE
     )
+  )
+  
+  
+  ## Works with species categories
+  # Single category 
+  expect_silent(
+    given_plot <- fb_plot_species_traits_completeness(
+      example_traits,
+      data.frame(species  = example_traits$species,
+                 category = "A"),
+      all_traits = FALSE
+    )
+  )
+  
+  # Less categories than species
+  expect_silent(
+    given_plot <- fb_plot_species_traits_completeness(
+      example_traits,
+      data.frame(species  = example_traits$species,
+                 category = c(1, 1, 2)),
+      all_traits = FALSE
+    )
+  )
+  
+  # As many categories as species
+  expect_silent(
+    given_plot <- fb_plot_species_traits_completeness(
+      example_traits,
+      data.frame(species  = example_traits$species,
+                 category = example_traits$species),
+      all_traits = FALSE
+    )
+  )
+  
+})
+
+test_that("fb_plot_species_traits_completeness() fails gracefully", {
+  
+  expect_error(
+    fb_plot_species_traits_completeness(species_traits, FALSE),
+    "'species_categories' isn't a two-column data.frame"
   )
   
 })
