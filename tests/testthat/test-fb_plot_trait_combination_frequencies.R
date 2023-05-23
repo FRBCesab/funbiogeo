3db5a0c8b99c4c43f05672cf33df647891dd45d5
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2023-05-23 09:15:30 +0200

    Add testing for fb_plot_trait_combination_frequenceies()

--- tests/testthat/test-fb_plot_trait_combination_frequencies.R
+++ tests/testthat/test-fb_plot_trait_combination_frequencies.R
@@ -6,8 +6,41 @@ test_that("fb_plot_trait_combination_frequencies() works", {
   expect_s3_class(res, "ggplot")
   
   expect_silent(
-    res <- fb_plot_trait_combination_frequencies(species_traits, "complete")
+    res <- fb_plot_trait_combination_frequencies(
+      species_traits, NULL, "complete"
+    )
   )
   
   expect_s3_class(res, "ggplot")
+  
+  
+  ## Works with species categories
+  # Single category 
+  expect_silent(
+    given_plot <- fb_plot_trait_combination_frequencies(
+      species_traits,
+      data.frame(species  = species_traits$species,
+                 category = "A")
+    )
+  )
+  
+  # Less categories than species
+  expect_silent(
+    given_plot <- fb_plot_trait_combination_frequencies(
+      species_traits,
+      data.frame(
+        species  = species_traits$species,
+        category = sample(letters[1:3], nrow(species_traits), replace = TRUE)
+      )
+    )
+  )
+  
+  # As many categories as species
+  expect_silent(
+    given_plot <- fb_plot_trait_combination_frequencies(
+      species_traits,
+      data.frame(species  = species_traits$species,
+                 category = species_traits$species)
+    )
+  )
 })
