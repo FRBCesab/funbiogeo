72ea17e3b6616fe8d9a391167a42e8760995a282
Nicolas Casajus <ahasverus@users.noreply.github.com>
2024-04-12 11:29:13 +0200

    replace example datasets + edit doc

--- R/datasets.R
+++ R/datasets.R
@@ -1,19 +1,26 @@
-#' Sites x Locations of Western Europe Mammals
+#' Sites x Locations data
 #' 
-#' This dataset contains spatial coordinates (grid cells of 0.5° x 0.5° 
-#' horizontal resolution) of sites of Western Europe where mammals species 
-#' presence/absence (see `site_species` dataset) has been sampled.
-#' 
-#' See \url{https://github.com/frbcesab/eumammals} for a complete description 
-#' of the workflow used to create this dataset.
+#' @description
+#' This dataset is derived from the WOODIV database (available at:
+#' \url{https://www.nature.com/articles/s41597-021-00873-3}). It contains the 
+#' spatial coordinates of sites (grid cells of 10 x 10 km  horizontal 
+#' resolution) sampled in Portugal, Spain, France, and Italy (Mediterranean 
+#' part) for which at least one of the 26 tree species (members of the Rosaceae
+#' family) occurs.
 #' 
 #' This dataset shows the format of the argument `site_locations` used in 
-#' several functions of `funbiogeo`.
-#' 
-#' The variable `site` corresponds to the sites labels.
+#' several functions of `funbiogeo`. The variable `site` corresponds to the 
+#' site labels (must match the same column in `site_species`) and the variable
+#' `country` will be used as a site grouping factor.
 #'   
-#' @format An `sf` object with 1,505 rows (grid cells) with a WGS84 (EPSG:4236)
-#'   Coordinate Reference System.
+#' @format An `sf` `POLYGON` object with 4,723 rows (grid cells) defined in the
+#'   ETRS89-extended / LAEA Europe Coordinate Reference System (EPSG:3035).
+#' 
+#' @references 
+#' Monnet AC, Cilleros K, Médail F _et al._ (2021) WOODIV, a database of 
+#' occurrences, functional traits, and phylogenetic data for all 
+#' Euro-Mediterranean trees. **Scientific Data**, 8, 89.
+#' DOI: [10.1038/s41597-021-00873-3](https://doi.org/10.1038/s41597-021-00873-3)
 #' 
 #' @examples 
 #' data("site_locations")
@@ -25,27 +32,28 @@
 
 
 
-#' Sites x Species of Western Europe Mammals
+#' Sites x Species data
 #' 
-#' This dataset contains the presence/absence of mammals species in Western
-#' Europe. This dataset is derived from IUCN range maps downloaded at:
-#' \url{https://www.iucnredlist.org/resources/spatial-data-download}. In 
-#' respect to the license, species names have been anonymized.
-#' 
-#' See \url{https://github.com/frbcesab/eumammals} for a complete description 
-#' of the workflow used to create this dataset.
+#' @description
+#' This dataset is derived from the WOODIV database (available at:
+#' \url{https://www.nature.com/articles/s41597-021-00873-3}). It contains the 
+#' presence/absence of the 26 tree species (members of the Rosaceae family) 
+#' occurring in Portugal, Spain, France, and Italy (Mediterranean part). The 
+#' presence/absence is available at 4,723 sites (grid cells of 10 x 10 km 
+#' horizontal resolution). Sites coordinates are provided in `site_locations`.
 #' 
 #' This dataset shows the format of the argument `site_species` used in 
-#' several functions of `funbiogeo`.
-#' 
-#' Note that sites labels (`site`) is the first column of this dataset.
+#' several functions of `funbiogeo`. Note that site labels must be the first 
+#' column and must be named `site`.
 #'   
-#' @format A `data.frame` with 1,505 rows (sites) and 150 columns (1 column for 
-#' site label and 149 for species occurrence).
+#' @format A `data.frame` with 4,723 rows (sites) and 27 columns (1 column for 
+#' site label and 26 for species occurrence).
 #' 
 #' @references 
-#' IUCN (2021) The IUCN Red List of Threatened Species. Version 2021-2.
-#' Downloaded on 2021/10/11.
+#' Monnet AC, Cilleros K, Médail F _et al._ (2021) WOODIV, a database of 
+#' occurrences, functional traits, and phylogenetic data for all 
+#' Euro-Mediterranean trees. **Scientific Data**, 8, 89.
+#' DOI: [10.1038/s41597-021-00873-3](https://doi.org/10.1038/s41597-021-00873-3)
 #' 
 #' @examples 
 #' data("site_species")
@@ -57,39 +65,34 @@
 
 
 
-#' Species x Traits of Western Europe Mammals
+#' Species x Traits data
 #' 
-#' This dataset contains values for six functional traits of Western Europe
-#' mammals species. These values were extracted from the **PanTHERIA** database
-#' (Jones 2009), a database on World mammals traits available at: 
-#' \url{https://esapubs.org/archive/ecol/E090/184/metadata.htm}.
-#' In respect to the IUCN license, species names have been anonymized.
-#' 
-#' See \url{https://github.com/frbcesab/eumammals} for a complete description 
-#' of the workflow used to create this dataset.
+#' @description
+#' This dataset is derived from the WOODIV database (available at:
+#' \url{https://www.nature.com/articles/s41597-021-00873-3}). It contains the 
+#' values of four functional traits for 26 tree species (members of the Rosaceae
+#' family) occurring in Portugal, Spain, France, and Italy (Mediterranean part).
 #' 
 #' This dataset shows the format of the argument `species_traits` used in 
-#' several functions of `funbiogeo`.
-#' 
-#' Note that species names (`species`) is the first column of this dataset.
+#' several functions of `funbiogeo`. Note that species names must be
+#' the first column and must be named `species`.
 #'   
-#' @format A matrix with 149 rows (species) and the following seven columns:
+#' @format A `data.frame` with 26 rows (species) and the following five columns:
 #' \describe{
 #'   \item{species}{species name corresponding to the columns of `site_species`}
-#'   \item{adult_body_mass}{adult body mass (in grams)}
-#'   \item{gestation_length}{length of time of non-inactive fetal growth 
-#'   (in days)}
-#'   \item{litter_size}{number of offspring born per litter per female}
-#'   \item{max_longevity}{maximum adult age (in months)}
-#'   \item{sexual_maturity_age}{age when individuals are first physically 
-#'   capable of reproducing (in days)}
-#'   \item{diet_breadth}{number of dietary categories eaten}
+#'   \item{plant_height}{adult plant height (in \eqn{m})}
+#'   \item{seed_mass}{seed mass (in \eqn{g})}
+#'   \item{sla}{specific leaf area, i.e. the ratio between leaf area and dry 
+#'   mass (in \eqn{m^{2}\cdot kg^{−1}})}
+#'   \item{wood_density}{wood density, i.e. stem specific density 
+#'   (in \eqn{kg\cdot dm^{–3}})}
 #' }
 #' 
 #' @references 
-#' Jones KE _et al._ (2009) PanTHERIA: A species-level database of life 
-#' history, ecology, and geography of extant and recently extinct mammals. 
-#' _Ecology_, **90**, 2648. DOI: 10.1890/08-1494.1.
+#' Monnet AC, Cilleros K, Médail F _et al._ (2021) WOODIV, a database of 
+#' occurrences, functional traits, and phylogenetic data for all 
+#' Euro-Mediterranean trees. **Scientific Data**, 8, 89.
+#' DOI: [10.1038/s41597-021-00873-3](https://doi.org/10.1038/s41597-021-00873-3)
 #' 
 #' @examples 
 #' data("species_traits")
@@ -101,27 +104,27 @@
 
 
 
-#' Species x Categories of Western Europe Mammals
+#' Species x Categories data
 #' 
-#' This dataset contains the order of mammals species in Western
-#' Europe. This dataset is derived from IUCN range maps downloaded at:
-#' \url{https://www.iucnredlist.org/resources/spatial-data-download}. In 
-#' respect to the license, species names have been anonymized.
-#' 
-#' See \url{https://github.com/frbcesab/eumammals} for a complete description 
-#' of the workflow used to create this dataset.
+#' @description
+#' This dataset is derived from the WOODIV database (available at:
+#' \url{https://www.nature.com/articles/s41597-021-00873-3}). It contains the 
+#' genus (and binomial name) of the 26 tree species (members of the Rosaceae
+#' family) occurring in Portugal, Spain, France, and Italy (Mediterranean part).
+#' The genus will be used as a species grouping factor.
 #' 
 #' This dataset shows the format of the argument `species_categories` used in 
-#' several functions of `funbiogeo`.
-#' 
-#' Note that species names (`species`) is the first column of this dataset.
+#' several functions of `funbiogeo`. Note that species names must be
+#' the first column and must be named `species`.
 #'   
-#' @format A `data.frame` with 149 rows (species) and 2 columns (1 column for 
-#' species name and 1 for the order).
+#' @format A `data.frame` with 26 rows (species) and 3 columns (`species`, 
+#' `genus`, `binomial`).
 #' 
 #' @references 
-#' IUCN (2021) The IUCN Red List of Threatened Species. Version 2021-2.
-#' Downloaded on 2021/10/11.
+#' Monnet AC, Cilleros K, Médail F _et al._ (2021) WOODIV, a database of 
+#' occurrences, functional traits, and phylogenetic data for all 
+#' Euro-Mediterranean trees. **Scientific Data**, 8, 89.
+#' DOI: [10.1038/s41597-021-00873-3](https://doi.org/10.1038/s41597-021-00873-3)
 #' 
 #' @examples 
 #' data("species_categories")
