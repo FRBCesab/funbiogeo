c328d89a9e1f8c6ee5a8c560da1a7214ea8c08e8
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2024-07-25 14:57:04 +0200

    Working version for trait coverage of sites

--- tests/testthat/test-fb_plot_distribution_site_trait_coverage.R
+++ tests/testthat/test-fb_plot_distribution_site_trait_coverage.R
@@ -13,12 +13,43 @@ test_that("fb_plot_distribution_site_trait_coverage() works", {
   expect_silent(
     suppressMessages(
       res <- fb_plot_distribution_site_trait_coverage(
-        site_species, species_traits, FALSE
+        site_species, species_traits, all_traits = FALSE
       )
     )
   )
   
   expect_s3_class(res, "ggplot")
+  
+  
+  ## Works with species categories
+  # Single category 
+  expect_silent(
+    suppressMessages(
+      res <- fb_plot_distribution_site_trait_coverage(
+        site_species, species_traits,
+        data.frame(species  = species_traits$species, category = "A")
+      )
+    )
+  )
+  
+  expect_s3_class(res, "ggplot")
+  
+  
+  # Less categories than species
+  expect_silent(
+    suppressMessages(
+      res <- fb_plot_distribution_site_trait_coverage(
+        site_species, species_traits,
+        data.frame(
+          species  = species_traits$species,
+          category = sample(letters[1:3], nrow(species_traits), replace = TRUE)
+        )
+      )
+    )
+  )
+  
+  expect_s3_class(res, "ggplot")
+  
 })
 
 test_that("ggridges installation is checked", {
