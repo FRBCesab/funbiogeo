4525e6431913472ccf06a475d3140327850edaee
Nicolas Casajus <rdev.nc@gmail.com>
2025-03-03 15:55:03 +0100

    better check species x categories object

--- R/checks.R
+++ R/checks.R
@@ -270,18 +270,36 @@ check_object_name <- function(object) {
 #'   the second column giving their corresponding categories
 #'
 #' @noRd
+
 check_species_categories <- function(species_categories) {
   
-  if (
-    !is.null(species_categories) &
-    (!is.data.frame(species_categories) | sum(ncol(species_categories)) != 2)
-  ) {
-    stop("'species_categories' isn't a two-column data.frame", call. = FALSE)
-  }
-
+  
+  # Only if it's not NULL ------------------------------------------------------
+  
   if (!is.null(species_categories)) {
+    
+    if (!is.data.frame(species_categories)) {
+      stop("The species x categories object must be a data.frame", 
+           call. = FALSE)
+    }
+
+    if (nrow(species_categories) < 1L) {
+      stop("The species x categories object should have at least one row", 
+           call. = FALSE)
+    }
+
+    if (ncol(species_categories) != 2L) {
+      stop("The species x categories object must have two columns (species ", 
+           "name and one category)", call. = FALSE)
+    }
+
+    if (is.null(colnames(species_categories))) {
+      stop("The species x categories object must have column names",
+           call. = FALSE)
+    }
+    
     if (!("species" %in% colnames(species_categories))) {
-      stop("The species x categories object must contain the 'species' column", 
+      stop("The species x categories object must contain the 'species' column",
            call. = FALSE)
     }
   }
