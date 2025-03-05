eaf0da9ee0f2be496b68ca9374777772804a63f1
Matthias Grenié <matthias.grenie@univ-grenoble-alpes.fr>
2025-03-05 21:40:12 +0100

    Add missing tests for check_species_categories()

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -236,6 +236,52 @@ test_that("check_site_locations() works", {
 })
 
 
+# Tests for check species-categories -------------------------------------------
+
+test_that("", {
+  
+  ## Wrong inputs
+  # Not good type of object
+  expect_error(
+    check_species_categories("a"),
+    "The species x categories object must be a data.frame",
+    fixed = TRUE
+  )
+  
+  # Not enough rows
+  expect_error(
+    check_species_categories(data.frame("a")[-1,, drop = FALSE]),
+    "The species x categories object should have at least one row",
+    fixed = TRUE
+  )
+  
+  # Not enough columns
+  expect_error(
+    check_species_categories(data.frame("a")),
+    paste0(
+      "The species x categories object must have two columns (species ", 
+      "name and one category)"
+    ),
+    fixed = TRUE
+  )
+  
+  # Not well named columns
+  expect_error(
+    check_species_categories(data.frame("a", "b")),
+    "The species x categories object must contain the 'species' column",
+    fixed = TRUE
+  )
+  
+  ## Working input
+  expect_silent(
+    check_species_categories(data.frame(species = "a", category = "plant"))
+  )
+  
+})
+
+
+# Tests for check threshold proportion -----------------------------------------
+
 test_that("check_threshold_proportion() works", {
   
   ## Wrong inputs
