0ca0d5f0e272d6718da78d63e3072fe83ab13eee
Nicolas Casajus <nicolas.casajus@gmail.com>
2025-03-06 12:09:33 +0000

    Triangular correlation plot (#122)
    * remove upper triangle in corr plot
    
    * update tests for corr plot

--- R/fb_plot_trait_correlation.R
+++ R/fb_plot_trait_correlation.R
@@ -21,45 +21,45 @@
 #' # Plot Spearman's correlation
 #' fb_plot_trait_correlation(woodiv_traits, method = "spearman")
 fb_plot_trait_correlation <- function(
-    species_traits, species_categories = NULL, ...
+  species_traits, species_categories = NULL, ...
 ) {
-  
+
   # Checks ---------------------------------------------------------------------
-  
+
   check_species_traits(species_traits)
   check_species_categories(species_categories)
-  
+
   # Keep only numeric columns
   numerical_traits <- vapply(
     species_traits[, -1, drop = FALSE], is.numeric, logical(1)
   )
-  
+
   if (!any(numerical_traits)) {
     stop("No numerical traits found, cannot plot trait correlations",
-         call. = FALSE)
+        call. = FALSE)
   }
-  
+
   if (!all(numerical_traits)) {
     message("Non-numerical traits found, only keeping numerical traits ",
             "to display trait correlations")
   }
-  
+
   # Subset Traits --------------------------------------------------------------
-  
+
   trait_subset <- species_traits[
     , c(
       "species", 
       colnames(drop_column(species_traits, "species"))[numerical_traits]
     ), drop = FALSE
   ]
-  
+
   species_traits_categories <- split_species_categories(
     trait_subset, species_categories
   )
-  
-  
+
+
   # Compute correlation --------------------------------------------------------
-  
+
   trait_cor <-  lapply(species_traits_categories, function(x, ...) {
     
     individual_cor <- as.data.frame(
@@ -68,16 +68,17 @@ fb_plot_trait_correlation <- function(
       )
     )
     
+    individual_cor[lower.tri(individual_cor, diag = FALSE)] <- NA
+    
     individual_cor[["first_trait"]] <- rownames(individual_cor)
     
     tidyr::pivot_longer(
       individual_cor, -"first_trait", names_to = "second_trait",
       values_to = "trait_cor"
     )
-    
   })
-  
-  
+
+
   # Manage conditional faceting
   if (is.null(species_categories)) {
     
@@ -103,41 +104,44 @@ fb_plot_trait_correlation <- function(
     )
     
   }
-  
+
   trait_cor <- do.call(rbind, trait_cor)
-  
-  
+  trait_cor <- trait_cor[!is.na(trait_cor$trait_cor), ]
+
   # Clean environment
   rm(species_traits, species_categories, species_traits_categories)
-  
-  
+
+
   # Actual Figure --------------------------------------------------------------
-  
+
   ggplot2::ggplot(
     trait_cor,
     ggplot2::aes(.data$first_trait, .data$second_trait, fill = .data$trait_cor)
   ) +
-    ggplot2::geom_raster() +
-    ggplot2::geom_text(
-      ggplot2::aes(label = round(.data$trait_cor, digits = 2))
-    ) +
-    category_facet +
-    # Re-order traits to have a matrix corresponding to what we compute
-    ggplot2::scale_x_discrete(
-      limits = unique(trait_cor$first_trait)
-    ) +
-    ggplot2::scale_y_discrete(
-      limits = rev(unique(trait_cor$first_trait))
-    ) +
-    # Cosmetic changes
-    ggplot2::scale_fill_distiller(
-      "Trait Correlation", palette = "PuOr", limits = c(-1, 1)
-    ) +
-    ggplot2::labs(x = NULL, y = NULL) +
-    ggplot2::coord_cartesian(expand = FALSE) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      aspect.ratio = 1,
-      legend.position ="top"
-    )
+  ggplot2::geom_raster() +
+  ggplot2::geom_text(
+    ggplot2::aes(label = round(.data$trait_cor, digits = 2))
+  ) +
+  category_facet +
+  # Re-order traits to have a matrix corresponding to what we compute
+  ggplot2::scale_x_discrete(
+    limits = unique(trait_cor$first_trait)
+  ) +
+  ggplot2::scale_y_discrete(
+    limits = rev(unique(trait_cor$first_trait))
+  ) +
+  # Cosmetic changes
+  ggplot2::scale_fill_distiller(
+    "Trait Correlation", palette = "PuOr", limits = c(-1, 1), na.value = NA
+  ) +
+  ggplot2::labs(x = NULL, y = NULL) +
+  ggplot2::coord_cartesian(expand = FALSE) +
+  ggplot2::theme_bw() +
+  ggplot2::theme(
+    aspect.ratio = 1,
+    legend.position ="top"
+  ) + 
+  ggplot2::guides(
+    fill = ggplot2::guide_colourbar(title.vjust = 0.8)
+  )
 }
