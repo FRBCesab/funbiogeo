dd20ef979d236d67d8bb622fd2557549a1a82b74
Nicolas Casajus <10423581+ahasverus@users.noreply.github.com>
2026-05-20 17:32:42 +0200

    test: skip spatial tests on fedora - fix #160

--- tests/testthat/test-fb_map_site_traits_completeness.R
+++ tests/testthat/test-fb_map_site_traits_completeness.R
@@ -1,7 +1,7 @@
 data("woodiv_site_species")
 data("woodiv_locations")
 data("woodiv_traits")
-site_species   <- woodiv_site_species
+site_species <- woodiv_site_species
 site_locations <- woodiv_locations
 species_traits <- woodiv_traits
 
@@ -14,108 +14,130 @@ site_lines <- sf::st_cast(site_locations, "MULTILINESTRING")
 
 # Actual tests -----------------------------------------------------------------
 test_that("fb_map_site_traits_completeness() works", {
-  
+  if (is_fedora()) {
+    skip("Skip tests on Fedora Linux")
+  }
+
   # With sf points
   expect_silent(
     given_plot <- fb_map_site_traits_completeness(
-      site_points, site_species, species_traits
+      site_points,
+      site_species,
+      species_traits
     )
   )
-  
+
   expect_s3_class(given_plot, "ggplot")
-  
+
   vdiffr::expect_doppelganger(
-    "fb_map_s_tr_comp-sfpts", 
+    "fb_map_s_tr_comp-sfpts",
     given_plot
   )
-  
+
   # With sf polygons
   expect_silent(
     given_plot <- fb_map_site_traits_completeness(
-      site_locations, site_species, species_traits
+      site_locations,
+      site_species,
+      species_traits
     )
   )
-  
+
   expect_s3_class(given_plot, "ggplot")
-  
+
   vdiffr::expect_doppelganger(
-    "fb_map_s_tr_comp-sfpoly", 
+    "fb_map_s_tr_comp-sfpoly",
     given_plot
   )
-  
+
   # with sf lines
   expect_silent(
     given_plot <- fb_map_site_traits_completeness(
-      site_lines, site_species, species_traits
+      site_lines,
+      site_species,
+      species_traits
     )
   )
-  
+
   expect_s3_class(given_plot, "ggplot")
-  
+
   vdiffr::expect_doppelganger(
-    "fb_map_s_tr_comp-sfml", 
+    "fb_map_s_tr_comp-sfml",
     given_plot
   )
-  
+
   # With sf points w/ background
   expect_silent(
     given_plot <- fb_map_site_traits_completeness(
-      site_points, site_species, species_traits, background = TRUE
+      site_points,
+      site_species,
+      species_traits,
+      background = TRUE
     )
   )
-  
+
   expect_s3_class(given_plot, "ggplot")
-  
+
   vdiffr::expect_doppelganger(
-    "fb_map_s_tr_comp-sfpt-w-bg", 
+    "fb_map_s_tr_comp-sfpt-w-bg",
     given_plot
   )
-  
+
   # With sf polygons w/ background
   expect_silent(
     given_plot <- fb_map_site_traits_completeness(
-      site_locations, site_species, species_traits, background = TRUE
+      site_locations,
+      site_species,
+      species_traits,
+      background = TRUE
     )
   )
-  
+
   expect_s3_class(given_plot, "ggplot")
-  
+
   vdiffr::expect_doppelganger(
-    "fb_map_s_tr_comp-sfpol-w-bg", 
+    "fb_map_s_tr_comp-sfpol-w-bg",
     given_plot
   )
-  
+
   # with sf lines w/ background
   expect_silent(
     given_plot <- fb_map_site_traits_completeness(
-      site_lines, site_species, species_traits, background = TRUE
+      site_lines,
+      site_species,
+      species_traits,
+      background = TRUE
     )
   )
-  
+
   expect_s3_class(given_plot, "ggplot")
-  
+
   vdiffr::expect_doppelganger(
-    "fb_map_s_tr_comp-sfml-w-bg", 
+    "fb_map_s_tr_comp-sfml-w-bg",
     given_plot
   )
-  
+
   # without all traits
   expect_silent(
     given_plot <- fb_map_site_traits_completeness(
-      site_lines, site_species, species_traits, FALSE
+      site_lines,
+      site_species,
+      species_traits,
+      FALSE
     )
   )
-  
+
   expect_s3_class(given_plot, "ggplot")
 })
 
 test_that("fb_map_site_traits_completeness() errors well", {
-  
   expect_error(
     fb_map_site_traits_completeness(
-      site_points, site_species, species_traits, background = 3
+      site_points,
+      site_species,
+      species_traits,
+      background = 3
     ),
     "The 'background' argument should either be TRUE or FALSE"
   )
-  
 })
