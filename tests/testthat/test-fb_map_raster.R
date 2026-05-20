1703f51053d98c36c6228a3edb2fa6c48ccb781e
Nicolas Casajus <10423581+ahasverus@users.noreply.github.com>
2026-05-20 17:32:42 +0200

    test: skip spatial tests on fedora - fix #160

--- tests/testthat/test-fb_map_raster.R
+++ tests/testthat/test-fb_map_raster.R
@@ -1,69 +1,69 @@
 data("woodiv_locations")
-site_locations   <- woodiv_locations
+site_locations <- woodiv_locations
 
-test_that("fb_map_raster() works", {
-  
-  prec   <- system.file("extdata", "annual_tot_prec.tif", 
-                        package = "funbiogeo")
-  tavg   <- system.file("extdata", "annual_mean_temp.tif", 
-                        package = "funbiogeo")
+test_that("fb_map_raster() errors", {
+  prec <- system.file("extdata", "annual_tot_prec.tif", package = "funbiogeo")
+  tavg <- system.file("extdata", "annual_mean_temp.tif", package = "funbiogeo")
   layers <- terra::rast(c(tavg, prec))
-  
-  
+
   ## Wrong input ----
-  
+
   expect_error(
     fb_map_raster(),
     "Argument 'x' is required",
     fixed = TRUE
   )
-  
+
   expect_error(
     fb_map_raster(site_locations),
     "Argument 'x' must be a 'SpatRaster' object (package terra)",
     fixed = TRUE
   )
-  
+
   expect_error(
     fb_map_raster(layers),
     "Argument 'x' must be a single layer 'SpatRaster' object",
     fixed = TRUE
   )
-  
+
   x <- terra::rast(prec)
-  
+
   expect_error(
     fb_map_raster(x, add = FALSE),
     NA
   )
-  
+
   expect_error(
     fb_map_raster(x, background = 3),
     "The 'background' argument should either be TRUE or FALSE"
   )
-  
-  
-  ## Working ----
-  
-  x <- terra::rast(prec)
-  
-  expect_silent(x <- fb_map_raster(x))
-  
-  expect_s3_class(x, "ggplot")
-  
-  vdiffr::expect_doppelganger(
-    "fb_map_raster-default", 
-    x
-  )  
+})
+
+
+test_that("fb_map_raster() works", {
+  if (is_fedora()) {
+    skip("Skip tests on Fedora Linux")
+  }
 
   x <- terra::rast(prec)
-  
-  expect_silent(y <- fb_map_raster(x, background = TRUE))
-  
-  expect_s3_class(y, "ggplot")
-  
+
+  expect_silent(x <- fb_map_raster(x))
+
+  expect_s3_class(x, "ggplot")
+
   vdiffr::expect_doppelganger(
-    "fb_map_raster-background", 
+    "fb_map_raster-default",
+    x
+  )
+
+  x <- terra::rast(prec)
+
+  expect_silent(y <- fb_map_raster(x, background = TRUE))
+
+  expect_s3_class(y, "ggplot")
+
+  vdiffr::expect_doppelganger(
+    "fb_map_raster-background",
     y
-  )  
+  )
 })
