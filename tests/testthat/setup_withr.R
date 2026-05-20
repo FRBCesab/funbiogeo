0625be86683915c9cf91e82c18be115083cd7b33
Nicolas Casajus <10423581+ahasverus@users.noreply.github.com>
2026-05-20 17:25:12 +0200

    test: add helper to detect fedora os

--- tests/testthat/setup_withr.R
+++ tests/testthat/setup_withr.R
@@ -1,13 +1,19 @@
 #' Setup Tests Infrastructure
-#' 
+#'
 
 ## Temporary Directory ----
 
 create_tempdir <- function(path = file.path(tempdir(), "fb")) {
-  
   withr::defer(fs::dir_delete(path), envir = parent.frame())
-  
+
   dir.create(path)
-  
+
   invisible(path)
+}
+
+
+## Detect Fedora OS (used to skip some spatial tests) ----
+
+is_fedora <- function() {
+  grepl("fedora", sessionInfo()$running, ignore.case = TRUE)
 }
